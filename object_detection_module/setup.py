367b47e4fe073977fd8b9516800a272eda3bf889
taeyoung730 <taeyoung730@github.com>
2025-06-05 09:18:38 +0900

    modify object_detection_module

--- object_detection_module/setup.py
+++ object_detection_module/setup.py
@@ -20,9 +20,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'yolo_detection_cam = object_detection_module.yolo_detection_cam:main',
-            'yolo_OCR_depth_checker0 = object_detection_module.yolo_OCR_depth_checker0:main',
-            'yolo_OCR_depth_checker1 = object_detection_module.yolo_OCR_depth_checker1:main',
+            'yolo_detection_cam0 = object_detection_module.yolo_detection_cam0:main',
+            'yolo_ocr_depth_checker0 = object_detection_module.yolo_ocr_depth_checker0:main',
+            'yolo_detection_cam1 = object_detection_module.yolo_detection_cam1:main',
+            'yolo_ocr_depth_checker1 = object_detection_module.yolo_ocr_depth_checker1:main',
 
         ],
     },
